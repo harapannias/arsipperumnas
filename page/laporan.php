6aae421f33f5218c03f62a3bf34504415cf77dc6
berkatharefa <dev.berkat@gmai.com>
2017-07-05 11:13:01 +0700

    Perubahan

---
+++ page/laporan.php
@@ -0,0 +1,57 @@
+<h3>Daftar laporan</h3>
+<hr>
+<table cellpadding="10">
+    <tr>
+        <td>
+            Filter Laporan
+        </td>
+        <td>
+            <select>
+                <option>Minggu</option>
+                <option>Bulan</option>
+                <option>Tahun</option>
+            </select>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            Filter Surat
+        </td>
+        <td>
+            <select>
+                <option>Surat Mask</option>
+                <option>Surat Keluar</option>
+            </select>
+        </td>
+    </tr>
+    <tr>
+        <td></td>
+        <td>
+            <a href="#" class="btn btn-success btn-xs">Filter</a>
+        </td>
+    </tr>
+</table>
+<p></p>
+<table class="table-hover">
+		<tr>
+			<th width="20px">No.</th>
+			<th>Nomor Surat</th>
+			<th>Tanggal Surat</th>
+			<th>Surat</th>
+            <th>Jenis Surat</th>
+		</tr>
+        <tr>
+			<td>1</td>
+			<td>123/PRMNS/VII/2017</td>
+			<td>10 Juli 2017</td>
+			<td>Keluar</td>
+			<td>Dinas</td>
+		</tr>
+        <tr>
+			<td>2</td>
+			<td>123/NISUT/x/2017</td>
+			<td>10 Juli 2017</td>
+			<td>Masuk</td>
+			<td>Dinas</td>
+		</tr>
+</table>
---
+++ page/surat_keluar.php
@@ -0,0 +1,54 @@
+<h3>Daftar Surat Keluar</h3>
+<hr>
+<p style="text-align: right;">
+		<a href="?page=tambah_data_operator" class="btn btn-success">Tambah Surat Keluar</a>
+</p>
+<table class="table-hover">
+		<tr>
+			<th width="20px">No.</th>
+			<th>Nomor Surat</th>
+			<th>Tanggal Surat</th>
+			<th>Tujuan</th>
+			<th width="90px">Action</th>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td>123/PRMNS/VII/2017</td>
+			<td>10 Juli 2017</td>
+			<td>Prof. Berkat Jaya Harefa S.COM</td>
+			<td>
+            <a href="?page=edit_data_operator&id=1" class="btn btn-success btn-xs">Edit</a>
+            <a href="?page=hapus_data_operator&id=1" class="btn btn-danger btn-xs" onclick="return confirm('Anda yakin ingin menghapus data?')">Hapus</a>
+            </td>
+		</tr>
+        <tr>
+			<td>1</td>
+			<td>123/PRMNS/VII/2017</td>
+			<td>10 Juli 2017</td>
+			<td>Prof. Berkat Jaya Harefa S.COM</td>
+			<td>
+            <a href="?page=edit_data_operator&id=1" class="btn btn-success btn-xs">Edit</a>
+            <a href="?page=hapus_data_operator&id=1" class="btn btn-danger btn-xs" onclick="return confirm('Anda yakin ingin menghapus data?')">Hapus</a>
+            </td>
+		</tr>
+        <tr>
+			<td>1</td>
+			<td>123/PRMNS/VII/2017</td>
+			<td>10 Juli 2017</td>
+			<td>Prof. Berkat Jaya Harefa S.COM</td>
+			<td>
+            <a href="?page=edit_data_operator&id=1" class="btn btn-success btn-xs">Edit</a>
+            <a href="?page=hapus_data_operator&id=1" class="btn btn-danger btn-xs" onclick="return confirm('Anda yakin ingin menghapus data?')">Hapus</a>
+            </td>
+		</tr>
+        <tr>
+			<td>1</td>
+			<td>123/PRMNS/VII/2017</td>
+			<td>10 Juli 2017</td>
+			<td>Prof. Berkat Jaya Harefa S.COM</td>
+			<td>
+            <a href="?page=edit_data_operator&id=1" class="btn btn-success btn-xs">Edit</a>
+            <a href="?page=hapus_data_operator&id=1" class="btn btn-danger btn-xs" onclick="return confirm('Anda yakin ingin menghapus data?')">Hapus</a>
+            </td>
+		</tr>
+</table>
