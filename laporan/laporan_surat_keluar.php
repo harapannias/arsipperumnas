70312ec29a2324ebaaceac7c6284100305609c7b
berkat <dev.berkat@gmail.com>
2017-08-30 15:01:20 +0700

    asd

--- laporan/laporan_surat_keluar.php
+++ laporan/laporan_surat_keluar.php
@@ -130,7 +130,7 @@ require('../config/koneksi.php');
 		$o->setY($y);
 		$o->setX(25);
 		$o->cell(170,10);
-		$o->cell(90,10,'NIP : 010100100100 10 10 1',0,0,'L',0);
+		$o->cell(90,10,'NIP : 01010010010 10 10 1',0,0,'L',0);
 
 	header('Content-type: application/pdf');
 	$o->Output();
